5222dc1cd9203487e65cba84636d571d5b55455f
thunguyetvo91-tech <thunguyet.vo.91@gmail.com>
2026-06-14 08:04:40 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    query,
+    orderBy,
+    limit
+}
+from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA-A10DAka_KKVeqltYyiPP1R8Az-eigxA",
@@ -10,6 +22,44 @@ const firebaseConfig = {
   measurementId: "G-62BJ91PM4F"
 };
 
-const app = initializeApp(firebaseConfig);
+const app =
+    initializeApp(firebaseConfig);
 
-console.log("Firebase loaded successfully");
+const db =
+    getFirestore(app);
+
+window.FirebaseDB = {
+
+    async saveScore(score){
+
+        await addDoc(
+            collection(
+                db,
+                "leaderboard"
+            ),
+            score
+        );
+    },
+
+    async getTopScores(max = 20){
+
+        const q = query(
+            collection(
+                db,
+                "leaderboard"
+            ),
+            orderBy(
+                "totalPoints",
+                "desc"
+            ),
+            limit(max)
+        );
+
+        const snapshot =
+            await getDocs(q);
+
+        return snapshot.docs.map(
+            doc => doc.data()
+        );
+    }
+};
